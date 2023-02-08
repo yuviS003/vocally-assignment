483c8eef64dc75e66b4d05975e2c0ad874431c2b
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-02-08 11:56:57 +0530

    swagger init

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const libraryRoutes = require("./routes/LibraryRoutes");
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUiExpress = require("swagger-ui-express");
+
 const app = express();
 
 app.use(cors());
@@ -22,6 +25,32 @@ mongoose
   });
 
 app.use("/library", libraryRoutes);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Vocally Library API docs",
+      version: "0.0.1",
+      description:
+        "This is a simple Library management API made with Node JS and Express JS",
+      contact: {
+        name: "Yuvraj Singh",
+        url: "https://github.com/yuviS003",
+        email: "yuvi7860808034@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:5000`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const spacs = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUiExpress.serve, swaggerUiExpress.setup(spacs));
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}`);
